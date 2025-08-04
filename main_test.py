b1a69476cf77d9f7a13acbe84fbc332191ba0f8e
ZaneMo <zhmo24@gmail.com>
2025-08-04 10:06:03 -0700

    just in case

--- main_test.py
+++ main_test.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import pandas as pd
 import openpyxl
 import glob
 import os
@@ -11,16 +10,7 @@ connector_path = "../excel/connector"
 cab_assem_path = "../excel/cable assembly"
 load_path = "../excel/load"
 
-# adapter_param_names = ['Connector 1 Type', 'Connector 1 Impedance', 'Connector 1 Polarity',
-#                    'Connector 2 Type', 'Connector 2 Impedance', 'Connector 2 Polarity',
-#                    'Connector Mount Method', 'Adapter Body Style', 'Frequency', 'Insertion Loss (dB)',
-#                    'VSWR /Return Loss', 'Center Contact', 'Outer Contact', 'Body', 'Dielectric',
-#                    'Temperature Range', 'Compliant']
-# adapter_param_match = [r'Connector\s*1\s*Type', r'Connector\s*1\s*Impedance', r'Connector\s*1\s*Polarity',
-#                     r'Connector\s*2\s*Type', r'Connector\s*2\s*Impedance', r'Connector\s*2\s*Polarity',
-#                     r'Connector\s*Mount\s*Method', r'Adapter\s*Body\s*Style', r'Frequency', r'Insertion\s*Loss\s*\(dB\)',
-#                     r'(?:VSWR\s*/\s*Return\s*Loss)|(?:Return\s*Loss\s*/\s*VSWR)', r'Cent(?:er|re)\s*Contact', r'Outer\s*Contact', r'Body',
-#                     r'Dielectric', r'Temperature\s*Range', r'Compliant']
+
 adapter_param_dict = {'Connector 1 Type': r'Connector\s*1\s*Type', 'Connector 1 Impedance': r'Connector\s*1\s*Impedance',
                    'Connector 1 Polarity': r'Connector\s*1\s*Polarity','Connector 2 Type': r'Connector\s*2\s*Type',
                    'Connector 2 Impedance': r'Connector\s*2\s*Impedance', 'Connector 2 Polarity':  r'Connector\s*2\s*Polarity',
@@ -30,14 +20,7 @@ adapter_param_dict = {'Connector 1 Type': r'Connector\s*1\s*Type', 'Connector 1 
                    'Center Contact': r'Cent(?:er|re)\s*Contact', 'Outer Contact': r'Outer\s*Contact',
                    'Body': r'Body', 'Dielectric': r'Dielectric','Temperature Range': r'Temperature\s*Range',
                    'Compliant': r'Compliant'}
-# connector_param_names = ['Connector 1 Type', 'Connector 1 Impedance', 'Connector 1 Polarity', 'Body Style',
-#                     'Connector Mount Method', 'Connector 2 Interface Type', 'Attachment Method','Frequency',
-#                     'Insertion Loss (dB)','VSWR /Return Loss', 'Center Contact', 'Outer Contact', 'Body', 'Dielectric',
-#                     'Temperature Range', 'Compliant']
-# connector_param_match = [r'Connector\s*1?\s*Type', r'Connector\s*1?\s*Impedance', r'Connector\s*1?\s*Polarity', r'Body\s*Style',
-#                     r'Connector\s*Mount\s*Method', r'Connector\s*2?\s*Interface\s*Type', r'Attachment\s*Method',r'Frequency',
-#                     r'Insertion\s*Loss\s*\(dB\)',r'(?:VSWR\s*/\s*Return\s*Loss)|(?:Return\s*Loss\s*/\s*VSWR)', r'Cent(?:re|er)\s*Contact',
-#                     r'Outer\s*Contact', r'Body', r'Dielectric', r'Temperature\s*Range', r'Compliant']
+
 connector_param_dict = {'Connector 1 Type': r'Connector\s*1?\s*Type', 'Connector 1 Impedance': r'Connector\s*1?\s*Impedance',
                      'Connector 1 Polarity': r'Connector\s*1?\s*Polarity', 'Body Style': r'Body\s*Style',
                      'Connector Mount Method': r'Connector\s*Mount\s*Method',
@@ -48,16 +31,7 @@ connector_param_dict = {'Connector 1 Type': r'Connector\s*1?\s*Type', 'Connector
                      'Center Contact': r'Cent(?:re|er)\s*Contact', 'Outer Contact': r'Outer\s*Contact',
                      'Body': r'Body', 'Dielectric': r'Dielectric', 'Temperature Range': r'Temperature\s*Range',
                      'Compliant': r'Compliant'}
-# cab_assem_param_names = ['Connector 1 Type','Connector 1 Body Style', 'Body Material and Plating','Connector 1 Mount Method',
-#                          'Connector 2 Type','Connector 2 Body Style', 'Body Material and Plating','Connector 2 Mount Method',
-#                          'Cable Type', 'Impedance', 'Frequency', 'Return Loss /VSWR', 'Phase Stability vs. Flexure',
-#                          'Amplitude Stability','Shielding Effectiveness','Phase Matching','Signal Delay','Power Handling',
-#                          'Temperature Range','Compliant']
-# cab_assem_param_match = [r'Connector\s*1\s*Type',r'Connector\s*1\s*Body\s*Style', r'Body\s*Material\s*and\s*Plating',r'Connector\s*1\s*Mount\s*Method',
-#                          r'Connector\s*2\s*Type',r'Connector\s*2\s*Body\s*Style', r'Body\s*Material\s*and\s*Plating',r'Connector\s*2\s*Mount\s*Method',
-#                          r'Cable\s*Type', r'Impedance', r'Frequency', r'(?:VSWR\s*/\s*Return Loss)|(?:Return Loss\s*/\s*VSWR)',
-#                          r'Phase\s*Stability\s*vs.\s*Flexure', r'Amplitude\s*Stability',r'Shielding\s*Effectiveness',r'Phase\s*Matching',
-#                          r'Signal\s*Delay',r'Power\s*Handling', r'Temperature\s*Range',r'Compliant']
+
 cab_assem_param_dict = {'Connector 1 Type': r'Connector\s*1\s*Type','Connector 1 Body Style': r'Connector\s*1\s*Body\s*Style',
                     'Connector 1 Body Material and Plating': r'Body\s*Material\s*and\s*Plating',
                     'Connector 1 Mount Method': r'Connector\s*1\s*Mount\s*Method',
@@ -70,11 +44,7 @@ cab_assem_param_dict = {'Connector 1 Type': r'Connector\s*1\s*Type','Connector 1
                     'Amplitude Stability': r'Amplitude\s*Stability','Shielding Effectiveness': r'Shielding\s*Effectiveness',
                     'Phase Matching': r'Phase\s*Matching','Signal Delay': r'Signal\s*Delay','Power Handling': r'Power\s*Handling',
                     'Temperature Range': r'Temperature\s*Range','Compliant': r'Compliant'}
-# load_param_names = ['Connector 1 Type', 'Connector 1 Impedance', 'Connector 1 Polarity','Frequency','Insertion Loss (dB)',
-#                     'VSWR /Return Loss','Power','Center Contact','Outer Contact','Body','Dielectric','Temperature Range','Compliant']
-# load_param_match = [r'Connector\s*1?\s*Type', r'Connector\s*1?\s*Impedance', r'Connector\s*1?\s*Polarity',r'Frequency',
-#                     r'Insertion\s*Loss\s*\(dB\)',r'(?:VSWR\s*/\s*Return\s*Loss)|(?:Return\s*Loss\s*/\s*VSWR)', r'Power',
-#                     r'Cent(?:re|er)\s*Contact',r'Outer\s*Contact',r'Body',r'Dielectric',r'Temperature\s*Range',r'Compliant']
+
 load_param_dict = {'Connector 1 Type': r'Connector\s*1?\s*Type', 'Connector 1 Impedance': r'Connector\s*1?\s*Impedance',
                 'Connector 1 Polarity': r'Connector\s*1?\s*Polarity','Frequency': r'Frequency',
                 'Insertion Loss (dB)':r'Insertion\s*Loss\s*\(dB\)',
